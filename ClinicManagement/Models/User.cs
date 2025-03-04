6b105a67fceff06bdb231329e3f55d00e4b3358c
rubbas <rubbasyarkhan007@gmail.com>
2025-03-04 21:12:37 +0500

    im batman

--- ClinicManagement/Models/User.cs
+++ ClinicManagement/Models/User.cs
@@ -15,7 +15,8 @@ public partial class User
 
     public string Role { get; set; } = null!;
 
-    public string FullName { get; set; } = null!;
+    public string? FullName { get; set; }
+
 
     public string? PhoneNumber { get; set; }
 
